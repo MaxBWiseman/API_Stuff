efa43fae2aef2a65471dfca39a4835276c5f8163
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-06 18:47:22 +0000

    feat: Add form submission functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,44 @@ const API_URL = 'https://ci-jshint.herokuapp.com/api';
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById('checksform'))
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form
+    })
+    
+    const data = await response.json()
+
+    if (response.ok) {
+        displayErrors(data)
+    } else {
+        throw new Error(data.error)
+    }
+}
+
+function displayErrors(data) {
+    
+    let heading = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class="no-errors">No errors reported!</div>`
+    }else {
+        results = `<div>Total Errors: ${data.total_errors}</div>`
+        for (let error of data.error_list) {
+            results += `<div>At line ${error.line}, column ${error.character}: ${error.reason}</div>`
+        }
+    }
+    document.getElementById("resultsModalTitle").innerHTML = heading;
+    document.getElementById('results-content').innerHTML = results;
+    resultsModal.show();
+}
 
 async function getStatus(e) {
 
@@ -17,15 +55,15 @@ async function getStatus(e) {
     } else {
         throw new Error(data.error);
     }
+}
 
 function displayStatus(data) {
-   let heading = 'API Key Status'
-   let results = `<div>Your key is valid until</div>`
-   results += `<div class='key-status'>${data.expiry}</div>`
+    let heading = 'API Key Status'
+    let results = `<div>Your key is valid until</div>`
+    results += `<div class='key-status'>${data.expiry}</div>`
 
-   document.getElementById("resultsModalTitle").innerHTML = heading;
-   document.getElementById('results-content').innerHTML = results;
+    document.getElementById("resultsModalTitle").innerHTML = heading;
+    document.getElementById('results-content').innerHTML = results;
 
     resultsModal.show();
-}
 }
