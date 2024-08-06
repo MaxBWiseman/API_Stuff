d40236aad58acf64b2583a8f8ace1df1114e61d9
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-06 18:06:18 +0000

    chore: Update API key and fetch data in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
-const API_KEY = XFxdlmdT09z9sNohEcCpCDcMilA
+const API_KEY = 'XFxdlmdT09z9sNohEcCpCDcMilA'
 const API_URL = 'https://ci-jshint.herokuapp.com/api';
-const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
-document.getElementById('status').addEventListener('click', e => getStatus(e));
+document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
-    const queryString = '${API_URL}?api_key=${API_KEY}';
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
 
     const data = await response.json();
 
-    
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+
 }
